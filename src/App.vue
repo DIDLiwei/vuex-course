5d1b1ead43c89620f0a846b11a75f085780faa2d
DIDLiwei <623790416@qq.com>
2019-08-24 16:23:22 +0800

    'lesson-2'

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
   <div id="app">
     <p>App.vue:{{count}}</p>
-    <p>App.vue:{{todos}}</p>
-    <Count />
+    <!-- <p>App.vue:{{todos}}</p> -->
+    <!-- <Count /> -->
+    <p>{{completedTodos}}</p>
+    <p>{{completedTodosCount}}</p>
+    <p>{{getTodosById(2)}}</p>
   </div>
 </template>
 
 <script>
 import Count from "./components/Count";
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 export default {
   name: "app",
   components: {
     Count
   },
-  computed: mapState(["count", "todos"])
-  // 写法二
-  // computed: mapState({
-  // count: state => state.count,
-  // count:function(state){
-  //   return state.count
-  // },
-  // todos: state => state.todos
-  // })
-  // 写法一
-  // count() {
-  //   return this.$store.state.count;
-  // },
-  // todos() {
-  //   return this.$store.state.todos;
+  computed: mapGetters([
+    "count",
+    "completedTodos",
+    "completedTodosCount",
+    "getTodosById"
+  ])
+  // computed: {
+  //   count() {
+  //     return this.$store.getters.getCount;
+  //   },
+  //   completedTodos() {
+  //     return this.$store.getters.completedTodos;
+  //   },
+  //   completedTodosCount() {
+  //     return this.$store.getters.completedTodosCount;
+  //   },
+  //   getTodosById() {
+  //     return this.$store.getters.getTodosById;
+  //   }
   // }
 };
 </script>
--- src/store.js
+++ src/store.js
@@ -15,17 +15,31 @@ export default new Vuex.Store({
             {
                 id: 2,
                 title: "todo item 2",
-                completed: false
+                completed: true
             },
             {
                 id: 3,
                 title: "todo item 3",
-                completed: false
+                completed: true
             }
         ]
     },
     getters: {
-
+        count: state => ++state.count,
+        // count(state) {
+        //     return ++state.count
+        // }
+        completedTodos: state => state.todos.filter(todo => todo.completed),
+        // completedTodos: function (state) {
+        //     return state.todos.filter(function (todo) {
+        //         return todo.completed
+        //     })
+        // }
+        // completedTodosCount:(state,getters) => getters.completedTodos.length
+        completedTodosCount: function(state, getters) {
+            return getters.completedTodos.length;
+        },
+        getTodosById: state => id => state.todos.find(todo => todo.id == id)
     },
     mutations: {
 
